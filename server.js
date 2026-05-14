87286931faba240fa71ad45e25eae329515da7ec
月名为曦 <14131025+the-name-of-the-moon-is-xi@user.noreply.gitee.com>
2026-05-15 01:21:32 +0800

    Add site favicon and brand icon

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ const contentTypes = {
   ".js": "application/javascript; charset=utf-8",
   ".html": "text/html; charset=utf-8",
   ".json": "application/json; charset=utf-8",
+  ".png": "image/png",
   ".txt": "text/plain; charset=utf-8",
   ".xml": "application/xml; charset=utf-8",
   ".ico": "image/x-icon",
